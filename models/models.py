8327f4d4e4346f0bd9dc620d007d37ad4ea8cac1
eliza-giane <egiane@oberlin.edu>
2024-12-29 02:58:10 -0300

    Added support for absolute value in the objective function

--- models/models.py
+++ models/models.py
@@ -6,8 +6,30 @@
 
 
 '''
-Model for creating a playlist in which the difference in tempo between each sequential track is minimal
+Model for creating a playlist in which the difference in tempo between each sequential track is minimal.
 '''
-def uniform_increasing_tempo(tracks):
+def uniform_tempo(tracks):
     pass
 
+'''
+Model for creating a playlist in which the difference in tempo between each sequential track is minimal and increasing.
+'''
+def uniform_increasing_tempo(tracks):
+    # DATA
+    # tempo value of each track 
+
+    # VARIABLES
+    # x_ij = |1 if track i is placed in position j of the playlist
+    #        |0 otherwise
+
+
+    # OBJECTIVE FUNCTION
+    # the sum of the absolute value of every pair of consecutive tracks
+    pass
+
+
+'''
+Model for creating a playlist in which the difference in tempo between each sequential track is minimal and decreasing.
+'''
+def uniform_decreasing_tempo(tracks):
+    pass
--- solve.py
+++ solve.py
@@ -1,26 +1,62 @@
 import cvxpy as cp
-import numpy as np
 
-
+'''
+Parses the information from the text file representation of the model.
+'''
 def read_file(path):
     with open(path, 'r') as model_file:
         lines = [line.strip() for line in model_file.readlines()]
 
-    n = lines[0].split(',')
-    num_constraints = int(n[0])
-    x_vars = (n[1:])
-
+    x_vars = lines[0].split(',')
     problem_type = lines[1]
     obj_exp = lines[2].split(',')
     constraint_exps = [line.split(',') for line in lines[3:]]
 
-    return x_vars, num_constraints, problem_type, obj_exp, constraint_exps
+    return x_vars, problem_type, obj_exp, constraint_exps
 
-def parse_constraints(x_vars, constraint_exps, num_vars, num_constraints):
+'''
+Creates the objective function cvxpy object.
+'''
+def parse_objective(obj_vars, x_vars, exp):
+    num_vars = len(x_vars)
+    obj = 0
+
+    i = 0 
+    while i < len(exp):
+
+        if exp[i] == '|':
+            abs_exp = 0
+            i += 1
+
+            while exp[i] != '|':
+                if exp[i][0] == '-':
+                    coef = int(exp[i][1:]) * (-1)
+                else:
+                    coef = int(exp[i])
+                abs_exp += coef * x_vars[obj_vars[i]]
+
+                i += 1
+
+            obj += cp.abs(abs_exp)
+
+        else:
+            if exp[i][0] == '-':
+                coef = int(exp[i][1:]) * (-1)
+            else:
+                coef = int(exp[i])
+                
+            obj += coef * x_vars[obj_vars[i]] 
+
+        i += 1
+      
+
+    return obj
+
+def parse_constraints(x_vars, constraint_exps):
+    num_vars = len(x_vars)
     constraints = []
     
-    for i in range(num_constraints):
-        exp = constraint_exps[i]
+    for exp in constraint_exps:
         rhs = int(exp[-1])
         op = exp[-2]
         lhs = 0
@@ -59,29 +95,30 @@ def parse_constraints(x_vars, constraint_exps, num_vars, num_constraints):
 # this is relevant to variable creation
 def solve(path):
     ### READ MODEL TEXT FILE ###
-    var_names,num_constraints,problem_type,obj_exp,constraint_exps = read_file(path)
+    obj_vars, problem_type, obj_exp, constraint_exps = read_file(path)
 
     ### CREATE VARIABLES ### 
-    num_vars=len(var_names)
+    # var_set = list(set(obj_vars))
+    # num_vars = len(var_set)
+
     x_vars = []
-    for i in range(num_vars):
-        x_vars.append(cp.Variable(integer=True, name=(var_names[i])))
+    x_dict = {}
+
+    
+    for i in range(len(obj_vars)):
+        if (obj_vars[i] not in x_dict.keys()) and (len(obj_vars[i]) > 0):
+            x = cp.Variable(integer=True, name=(obj_vars[i]))
+            x_vars.append(x)
+
+            x_dict[obj_vars[i]] = x
 
 
     ### PARSE OBJECTIVE FUNCTION ###
-    obj = 0
-    coef = 0
-    for i in range(len(x_vars)):   
-        if obj_exp[i][0] == '-':
-            coef = int(obj_exp[i][1:]) * (-1)
-        else:
-            coef = int(obj_exp[i])
-
-        obj += (coef * x_vars[i])
+    obj = parse_objective(obj_vars, x_dict, obj_exp)
 
 
     ### PARSE CONSTRAINTS ###
-    constraints = parse_constraints(x_vars,constraint_exps,num_vars, num_constraints)
+    constraints = parse_constraints(x_vars, constraint_exps)
 
 
     ### CREATE MODEL ###
@@ -96,9 +133,11 @@ def solve(path):
 
     ### RETURN RESULTS ###
     model_obj = str(problem.objective).replace(' @ ','')
+    num_constraints = len(constraints)
     model_constraints = [str(c).replace(' @ ','').replace(' <= ', ' \\leq ').replace(' >= ',' \\geq ') for c in problem.constraints[:num_constraints]]
     opt_val = problem.value
     x_solutions = [[var.name(),int(var.value)] for var in x_vars]
+
 
     return model_obj, model_constraints, opt_val, x_solutions
 
